6bcf1c04118846e05ca17d13d3bc4569b6f57aa4
Samuel <s-bridge@live.co.uk>
2015-10-13 20:51:45 +0100

    [JS] - Stripped down to basics

--- js/src/ticky_init.js
+++ js/src/ticky_init.js
@@ -1,16 +1,1 @@
-$('.picky').picky({
-
-	disable: ['2015-01-08', ['2015-01-16', '2015-01-18'], '2015-01-14', '2015-08-02', ['2015-08-06', '2015-08-10']],
-	disablePast: true,
-	disableFuture: 10,
-	disableDays: [],
-	labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-	advance: 0,
-	select_callback: function(input, cell, date) {
-
-		console.log(input, cell, date);
-
-	},
-	linked: $('.picky2')
-
-});
+$('.ticky').ticky();
